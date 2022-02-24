23ab0637427548a94182affa5f1916219311272b
JohnPena0115 <calmwinds06@gmail.com>
2022-02-24 17:44:51 -0500

    "Uploading progression."

--- src/com/careerdevs/UI.java
+++ src/com/careerdevs/UI.java
@@ -6,17 +6,17 @@ public class UI {
 
     private static Scanner read = new Scanner(System.in);
 
-    public static int readInt (String question, int min, int max) {
+    public static int readInt(String question, int min, int max) {
 
         System.out.print(question);
         System.out.print("\nSelection(" + min + " - " + max + "): ");
 
-        while(true){
+        while (true) {
 
-            try{
+            try {
 
                 int optionSelected = Integer.parseInt(read.nextLine());
-                if(optionSelected >= min && optionSelected<= max){
+                if (optionSelected >= min && optionSelected <= max) {
 
                     return optionSelected;
                 }
@@ -24,11 +24,52 @@ public class UI {
                 System.out.print("\nNumber outside of the valid range, please try again: ");
 
 
-            }catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
 
                 System.out.print("\nPlease only enter a valid integer: ");
 
             }
         }
     }
+
+    public static char confirmAlphabeticChar(String question) {
+
+        System.out.println("\n" + question);
+
+        String preValidatedChar = read.nextLine().trim();
+
+        //Filters for null input
+        while (true) {
+
+            if (preValidatedChar.length() > 0) {
+
+                break;
+
+            } else {
+
+                System.out.println("Try again. You many have pressed the enter button without typing in a letter.");
+
+            }
+        }
+
+        //Filters for non-char input exclusive of null
+        while (true) {
+
+            if (preValidatedChar.length() > 1) {
+
+                System.out.println("Please type in only one character.");
+
+            } else {
+
+                break;
+            }
+        }
+
+        while (true) {
+
+
+        }
+
+    }
+
 }
